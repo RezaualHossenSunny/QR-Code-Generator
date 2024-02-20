50983f8950966f10083a130b1ff9c38863cd600f
RezaualHossenSunny <91420025+RezaualHossenSunny@users.noreply.github.com>
2024-02-20 22:53:47 +0600

    project done

--- index.js
+++ index.js
@@ -6,6 +6,15 @@ const qrtext =document.getElementById("qrtext");
 
 
 function genarateqr(){
-  imgqr.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="+qrtext.value;
-  imgbox.classList.add("show-img")
+    if(qrtext.value.length >0){
+        imgqr.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="+qrtext.value;
+        imgbox.classList.add("show-img")
+    }else{
+        qrtext.classList.add("eroor");
+
+        setTimeout(()=>{
+        qrtext.classList.remove("eroor")
+        },1000)
+    }
+ 
 }
