965ec6e53e6491f28f1b85ed1d7a868463b0dac5
RezaualHossenSunny <91420025+RezaualHossenSunny@users.noreply.github.com>
2024-02-20 22:41:46 +0600

    degian implement

--- index.js
+++ index.js
@@ -6,5 +6,6 @@ const qrtext =document.getElementById("qrtext");
 
 
 function genarateqr(){
-  imgqr.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="+qrtext.value
+  imgqr.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="+qrtext.value;
+  imgbox.classList.add("show-img")
 }
