4752bf675f5ab52bb229d187e1bfa9947bde5803
RezaualHossenSunny <91420025+RezaualHossenSunny@users.noreply.github.com>
2024-02-20 22:09:28 +0600

    qr code link add

---
+++ index.js
@@ -0,0 +1,10 @@
+const imgbox =document.getElementById("imgbox");
+const imgqr =document.getElementById("imgqr");
+const qrtext =document.getElementById("qrtext");
+
+
+
+
+function genarateqr(){
+  imgqr.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="+qrtext.value
+}
